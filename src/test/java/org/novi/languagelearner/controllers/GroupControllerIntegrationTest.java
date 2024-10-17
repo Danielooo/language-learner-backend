a3d4105e485a433cd0973568cb1d22dff2d69a91
Danielooo <dvanraffe@gmail.com>
2024-10-17 17:40:37 +0200

    finished integration tests

--- src/test/java/org/novi/languagelearner/controllers/GroupControllerIntegrationTest.java
+++ src/test/java/org/novi/languagelearner/controllers/GroupControllerIntegrationTest.java
@@ -23,6 +23,7 @@ import org.springframework.http.MediaType;
 
 import java.util.Collections;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -59,15 +60,51 @@ public class GroupControllerIntegrationTest {
     }
 
     @Test
-    @WithMockUser(username = "Chef")
+    @WithMockUser(username = "testUser")
     public void createGroup() throws Exception {
         mockMvc.perform(post("/groups/user/create")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestJson))
                 .andExpect(status().isOk())
-                .andExpect()
-                );
+                .andExpect(jsonPath("$.groupName").value("Test Group"))
+                .andExpect(jsonPath("$.userName").value("testUser"))
+                .andExpect(jsonPath("$.exercises[0].question").value("Question 1"))
+                .andExpect(jsonPath("$.exercises[0].answer").value("Answer 1"))
+                .andExpect(jsonPath("$.exercises[1].question").value("Question 2"))
+                .andExpect(jsonPath("$.exercises[1].answer").value("Answer 2"));
+    }
 
+
+    @Test
+    @WithMockUser(username = "testUser")
+    public void addExercisesToGroup() throws Exception {
+        String requestJson = """
+            {
+                "id": 1,
+                "groupName": "Initial Test Group",
+                "userName": "testUser",
+                "exercises": [
+                    {
+                        "question": "New Question 1",
+                        "answer": "New Answer 1"
+                    },
+                    {
+                        "question": "New Question 2",
+                        "answer": "New Answer 2"
+                    }
+                ]
+            }""";
+
+        mockMvc.perform(patch("/groups/user/add-exercises")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.groupName").value("Initial Test Group 1"))
+                .andExpect(jsonPath("$.userName").value("testUser"))
+                .andExpect(jsonPath("$.exercises[2].question").value("New Question 1"))
+                .andExpect(jsonPath("$.exercises[2].answer").value("New Answer 1"))
+                .andExpect(jsonPath("$.exercises[3].question").value("New Question 2"))
+                .andExpect(jsonPath("$.exercises[3].answer").value("New Answer 2"));
     }
 }
 
--- src/test/resources/data-test.sql
+++ src/test/resources/data-test.sql
@@ -1,36 +1,27 @@
-
-
-
 -- ROLLEN
 insert into roles (active, description, role_name) values (true, 'administrator roles' , 'ROLE_ADMIN');
 insert into roles (active, description, role_name) values (true, 'user roles' , 'ROLE_USER');
 
 ---------------------
 
--- USERS
+-- USERS insert
 -- password unencrypted is 'password'
--- Kim  (userId = 1)
+-- testUser  (userId = 1)
 insert into users (password, user_name, first_name, last_name, are_credentials_expired, is_enabled, is_expired, is_locked)
-values ('$2a$12$41tSm.wI7.UK5YvM8d9wVOPpHMmz9OqgcZPZvno.Scsa6WT/kceLm','Chef', 'Kim', 'de Groot', false,true,false,false);
+values ('$2a$12$41tSm.wI7.UK5YvM8d9wVOPpHMmz9OqgcZPZvno.Scsa6WT/kceLm','testUser', 'Kim', 'de Groot', false,true,false,false);
 
--- Jelle  (userId = 2)
-insert into users (password, user_name, first_name, last_name, are_credentials_expired, is_enabled, is_expired, is_locked)
-values ('$2a$12$41tSm.wI7.UK5YvM8d9wVOPpHMmz9OqgcZPZvno.Scsa6WT/kceLm','Jelle', 'Jelle', 'Middendorp', false, true,false,false);
 
--- Bert  (userId = 3)
-insert into users (password, user_name, first_name, last_name, are_credentials_expired, is_enabled, is_expired, is_locked)
-values ('$2a$12$41tSm.wI7.UK5YvM8d9wVOPpHMmz9OqgcZPZvno.Scsa6WT/kceLm','Bert', 'Bert', 'Klein', false,true,false,false);
 
----------------------
-
--- ROLLEN
--- Kim is admin en user
+-- ROLLEN insert
+-- testUser is admin and user
 insert into user_role (role_id, user_id) values (1,1);
 insert into user_role (role_id, user_id) values (2,1);
 
--- Jelle is admin
-insert into user_role (role_id, user_id) values (1,2);
+---------------------
 
--- Bert is user
-insert into user_role (role_id, user_id) values (2,3);
+-- GROUPS
+insert into groups (user_id, group_name) values (1, 'Initial Test Group 1');
 
+-- EXERCISES
+insert into exercises (question, answer, group_id) values ('Initial Question 1', 'Initial Answer 1', (select id from groups where group_name = 'Initial Test Group 1'));
+insert into exercises (question, answer, group_id) values ('Initial Question 2', 'Initial Answer 2', (select id from groups where group_name = 'Initial Test Group 1'));
